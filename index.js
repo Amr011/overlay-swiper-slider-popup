e539fa0d33d43abb15e200b7eebd65192104d877
Amr109 <grand.109.master@gmail.com>
2023-05-31 10:00:59 +0400

    Show Side By Index

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 // Get the modal
 var sliderModal = document.getElementById('mySliderModal')
 
-// When the user clicks the button, open the modal
+var galleryTop
+var galleryThumbs
+
+function initializeSwiper() {
+  galleryTop = new Swiper('.gallery-top', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: 5,
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+
+    breakpoints: {
+      640: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 10,
+      },
+    },
+  })
+
+  galleryTop.controller.control = galleryThumbs
+  galleryThumbs.controller.control = galleryTop
+}
+
 function openSliderModal() {
   sliderModal.style.display = 'block'
+  initializeSwiper() // Call the function to initialize Swiper
+}
+
+function showSlideByIndex(sliderIndex) {
+  openSliderModal()
+  galleryTop.slideTo(sliderIndex)
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -18,38 +64,48 @@ window.onclick = function (event) {
   }
 }
 
-var galleryTop = new Swiper('.gallery-top', {
-  slidesPerView: 1,
-  spaceBetween: 10,
+// // Initialize Main Gallery Slider
+// let galleryTop = new Swiper('.gallery-top', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   centeredSlides: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// })
 
-  centeredSlides: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})
+// // Initialize Sub Gallery Slider
+// let galleryThumbs = new Swiper('.gallery-thumbs', {
+//   spaceBetween: 10,
+//   centeredSlides: true,
+//   slidesPerView: 5,
+//   touchRatio: 0.2,
+//   slideToClickedSlide: true,
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-  spaceBetween: 10,
-  centeredSlides: true,
-  slidesPerView: 5,
-  touchRatio: 0.2,
-  slideToClickedSlide: true,
-  breakpoints: {
-    640: {
-      slidesPerView: 5,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 6,
-      spaceBetween: 10,
-    },
-  },
-})
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 5,
+//       spaceBetween: 10,
+//     },
+//     768: {
+//       slidesPerView: 5,
+//       spaceBetween: 10,
+//     },
+//     1024: {
+//       slidesPerView: 6,
+//       spaceBetween: 10,
+//     },
+//   },
+// })
 
-galleryTop.controller.control = galleryThumbs
-galleryThumbs.controller.control = galleryTop
+// // When the user clicks the button, open the modal
+// function openSliderModal() {
+//   sliderModal.style.display = 'block'
+// }
+
+// // Show Slider By Index
+// function showSlideByIndex(sliderIndex) {
+//   openSliderModal()
+//   galleryTop.slideTo(sliderIndex)
+// }
